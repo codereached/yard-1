eee25f59ae83cd10ee8c19f44d2b63d6ff369b52
Quinn Slack <qslack@qslack.com>
2014-07-13 11:24:14 -0700

    use correct AST node (not current statement)

--- lib/yard/handlers/base.rb
+++ lib/yard/handlers/base.rb
@@ -546,8 +546,8 @@ module YARD
       # @return [void]
       def register_ast_node(object, ast_node = statement, type = parser.parser_type)
         object.ast_node ||= ast_node
-        if ast_node.respond_to?(:source_range)
-          YARD::Registry.obj_by_ast_node[YARD::Registry._ast_key(ast_node)] = object
+        if object.ast_node.respond_to?(:source_range)
+          YARD::Registry.obj_by_ast_node[YARD::Registry._ast_key(object.ast_node)] = object
         end
       end
 
