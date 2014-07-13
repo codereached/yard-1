3b6336e9b1e73254b5837f3a79fdae3b6f510148
Quinn Slack <qslack@qslack.com>
2014-07-13 15:53:08 -0700

    no ruby src tests by default

--- run_sourcegraph_tests.sh
+++ run_sourcegraph_tests.sh
@@ -1,3 +1,3 @@
 #export RUBY_SRC=~/.rvm/src/"$RUBY_VERSION"
-export RUBY_SRC=/tmp/sg/github.com/ruby/ruby
+#export RUBY_SRC=/tmp/sg/github.com/ruby/ruby
 rspec -c -f d -e 'TypeInf' && rspec -c -f d spec/handlers/reference_handler_spec.rb && rspec -c -f d spec/handlers/local_variable_handler_spec.rb
