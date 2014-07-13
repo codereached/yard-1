f5b109b515cff7281d800cc21415825062f6015f
Quinn Slack <qslack@qslack.com>
2014-07-13 13:50:57 -0700

    fix check

--- lib/yard/handlers/ruby/block_handler.rb
+++ lib/yard/handlers/ruby/block_handler.rb
@@ -6,7 +6,7 @@ class YARD::Handlers::Ruby::BlockHandler < YARD::Handlers::Ruby::Base
 
   process do
     body_scope = owner.new_local_scope("@block", local_scope)
-    handle_params(statement[0][0], body_scope) if statement[0][0]
+    handle_params(statement[0][0], body_scope) if statement[0]
     parse_block(statement[1], :owner => owner, :namespace => namespace, :local_scope => body_scope)
   end
 end
